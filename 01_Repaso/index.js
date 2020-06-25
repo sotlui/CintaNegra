7059f739d31ff0f3806b9133a613e991c43cad03
Lui Soto <sotlui@gmail.com>
2020-06-24 19:44:08 -0500

    archivo para ignorar

--- 01_Repaso/index.js
+++ 01_Repaso/index.js
@@ -32,11 +32,37 @@ request.get(api_url, (err, res, body) => {
     }
 });
  */
-const END_POINT_MUSCI = 'https://www.theaudiodb.com/api_guide.php';
+/* const API_KEY = '1';
+const ARTIST = 'Bon Jovi'
+const END_POINT_MUSCI = `https://www.theaudiodb.com/api/v1/json/${API_KEY}/search.php?s=${ARTIST}`;
 
 request.get(END_POINT_MUSCI, (err, res, body) => {
+    if (res.statusCode === 200) {
+        const json = JSON.parse(body);
+        console.log(json.artists[0].strBiographyES);
+    } else {
+        console.log(res.statusCode, err);
 
-});
+    }
+}); */
 
 
 //const api_finace = 'https://developer.tradier.com/';
+const TOKEN=''
+request({
+    method: 'post',
+    url: 'https://sandbox.tradier.com/v1/oauth/accesstoken',
+    form: {
+       'grant_type': 'authorization_code',
+       'code': 'PRpnf1o7'
+    },
+    headers: {
+      'Authorization': '<TOKEN>',
+      'Accept': 'application/json'
+    }
+  }, (error, response, body) => {
+      console.log(response.statusCode);
+      console.log(body);
+  });
+
+
