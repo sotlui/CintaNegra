5ac700030ad4ed1d2ec67ede9d0bf243c2804a49
Lui Soto <sotlui@gmail.com>
2020-06-26 20:22:53 -0500

    instalar dotenv recuerda agregar el archiov .env

--- 03_DataBase/index.js
+++ 03_DataBase/index.js
@@ -1,8 +1,18 @@
 //Configuracion
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//Conectar a nuestra base de datos
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Database connected'))
+    .catch(() => console.log('Error connecting'));
+
+
+
 
 //Endpoints
 app.get('/', (req, res) => res.status(200).json({ messaje: " it's alive!" }));
